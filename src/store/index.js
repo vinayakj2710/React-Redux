747143918648071a82e2111a47e34e27d2a6bf2e
Vinayak Jadhav <vinayak.jadhav7991@gmail.com>
2023-06-13 11:43:11 +0530

    Adding payload to the actions

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,17 @@
 import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type == 'increment') {
+    if (action.type === 'increment') {
         return {
             counter: state.counter + 1
         }
+  }
+  if (action.type === 'increase') {
+    return {
+      counter : state.counter + action.amount
     }
-    if (action.type == "decrement") {
+  }
+    if (action.type === "decrement") {
       return {
         counter: state.counter - 1,
       };
